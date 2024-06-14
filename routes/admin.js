7fd8666661d5a63de8dc7e61713b191aa7af2c7c
zeyad <zeiadabdelnasserelzayaty@gmail.com>
2024-06-14 16:21:01 +0300

    #admin routes , forgetpass , login

--- routes/admin.js
+++ routes/admin.js
@@ -4,9 +4,16 @@ const router = express.Router();
 
 
 router.get('/', (req, res) => {
+    res.render('Admin/Login');
+});
+
+router.get('/dashboard', (req, res) => {
     res.render('Admin/Dashboard');
 });
 
+router.get('/forgetpassword', (req, res) => {
+    res.render('Admin/Forgetpass');
+});
 
 module.exports = router;
 
